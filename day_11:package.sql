d14de73ac1172afde64abbada2528f7d9fb1f440
Muhajirin <muhajirinlpu@gmail.com>
2020-06-02 00:45:36 +0700

    day 11 solved

---
+++ day_11:package.sql
@@ -0,0 +1,44 @@
+CREATE OR REPLACE PACKAGE akuntansi IS
+    PROCEDURE tambah_gaji(emp_no in EMPLOYEES.EMPLOYEE_ID%TYPE, percentage_inc in NUMBER);
+    PROCEDURE hitung_pajak(emp_no in EMPLOYEES.EMPLOYEE_ID%TYPE);
+END akuntansi;
+
+CREATE OR REPLACE PACKAGE BODY akuntansi IS
+    PROCEDURE tambah_gaji(emp_no IN EMPLOYEES.EMPLOYEE_ID%TYPE, percentage_inc IN NUMBER)
+    IS
+        e_invalid_emp_no EXCEPTION;
+        e_name employees.LAST_NAME%type := '';
+    BEGIN
+        UPDATE EMPLOYEES
+            SET SALARY = SALARY + (percentage_inc / 100 * SALARY)
+        WHERE EMPLOYEE_ID = emp_no RETURNING LAST_NAME INTO e_name;
+
+        IF SQL%NOTFOUND THEN
+          RAISE e_invalid_emp_no;
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('Pegawai dengan nomor pegawai ' || emp_no || ' nama ' || e_name ||
+                             ' telah dinaikkan gajinya sebesar ' || percentage_inc || '%');
+        END IF;
+
+        EXCEPTION
+            WHEN e_invalid_emp_no THEN
+              DBMS_OUTPUT.PUT_LINE('ERROR: ' || emp_no || ' is an invalid emp_no.');
+    END tambah_gaji;
+
+    PROCEDURE hitung_pajak(emp_no in EMPLOYEES.EMPLOYEE_ID%type)
+    IS
+        salary NUMBER := 0;
+        tax NUMBER := 0;
+    BEGIN
+        select SALARY, case
+                   when SALARY <= 5000 then 15
+                   when SALARY <= 10000 then 20
+                   else 25
+                end
+        into salary, tax
+        from EMPLOYEES
+        where EMPLOYEE_ID = emp_no;
+
+        DBMS_OUTPUT.PUT_LINE(tax / 100 * salary);
+    END hitung_pajak;
+END akuntansi;
