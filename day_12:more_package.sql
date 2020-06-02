8fbec113ec9a225075959f10480140dff4b3d690
Muhajirin <muhajirinlpu@gmail.com>
2020-06-02 22:50:24 +0700

    module day_12

---
+++ day_12:more_package.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE PACKAGE pegawai IS
+    PROCEDURE cari_pegawai(dept_no in EMPLOYEES.LAST_NAME%type);
+    PROCEDURE cari_pegawai(last_name_q in EMPLOYEES.LAST_NAME%type);
+END pegawai;
+
+CREATE OR REPLACE PACKAGE BODY pegawai IS
+    PROCEDURE cari_pegawai(dept_no in EMPLOYEES.LAST_NAME%type) IS
+    BEGIN
+        for row in (select FIRST_NAME, LAST_NAME from EMPLOYEES where DEPARTMENT_ID = dept_no) loop
+            dbms_output.put_line(row.FIRST_NAME||' '||row.LAST_NAME);
+        end loop;
+    END cari_pegawai;
+    PROCEDURE cari_pegawai(last_name_q in EMPLOYEES.LAST_NAME%type) IS
+    BEGIN
+        for row in (select FIRST_NAME, LAST_NAME from EMPLOYEES where UPPER(LAST_NAME) like '%'||UPPER(last_name_q)||'%') loop
+            dbms_output.put_line(row.FIRST_NAME||' '||row.LAST_NAME);
+        end loop;
+    END cari_pegawai;
+END pegawai;
