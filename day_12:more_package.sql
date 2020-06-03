a706308a1a7db3fe8076c9292d2a1a701a876866
Julardo <julardos@it.student.pens.ac.id>
2020-06-03 14:25:01 +0700

    change prak 12 type

--- day_12:more_package.sql
+++ day_12:more_package.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE PACKAGE pegawai IS
-    PROCEDURE cari_pegawai(dept_no in EMPLOYEES.LAST_NAME%type);
+    PROCEDURE cari_pegawai(dept_no in EMPLOYEES.DEPARTMENT_ID%type);
     PROCEDURE cari_pegawai(last_name_q in EMPLOYEES.LAST_NAME%type);
 END pegawai;
-
+/
 CREATE OR REPLACE PACKAGE BODY pegawai IS
-    PROCEDURE cari_pegawai(dept_no in EMPLOYEES.LAST_NAME%type) IS
+    PROCEDURE cari_pegawai(dept_no in EMPLOYEES.DEPARTMENT_ID%type) IS
     BEGIN
         for row in (select FIRST_NAME, LAST_NAME from EMPLOYEES where DEPARTMENT_ID = dept_no) loop
             dbms_output.put_line(row.FIRST_NAME||' '||row.LAST_NAME);
